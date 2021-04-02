4cdebdd49191b474f0e5bbc05908d6923863e663
Rakibur Rahman <rakibur5454@gmail.com>
2021-04-02 22:04:14 +0600

    env variable added

--- index.js
+++ index.js
@@ -67,6 +67,5 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(process.env.PORT ||port)
+   
